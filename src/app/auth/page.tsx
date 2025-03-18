a1f84f94f8651c83d2c50712aa76e3d501d8b019
CnoLvaN <anordgame@gmail.com>
2025-03-18 01:29:37 +0100

    auth page styles

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -7,7 +7,6 @@ import { useRouter } from "next/navigation";
 import styles from "./page.module.scss";
 
 const AuthPage = () => {
-  const [isLogin, setIsLogin] = useState(true);
   const [loginData, setLoginData] = useState({ email: "", password: "" });
   const [signUpData, setSignUpData] = useState({
     name: "",
@@ -16,8 +15,8 @@ const AuthPage = () => {
     email: "",
     password: "",
   });
-  const [message, setMessage] = useState("");
   const router = useRouter();
+  const [isRightPanelActive, setIsRightPanelActive] = useState(false);
 
   useEffect(() => {
     const token = localStorage.getItem("userToken");
@@ -34,7 +33,6 @@ const AuthPage = () => {
       router.push("/home");
     } catch (error) {
       console.error(error);
-      setMessage("Ошибка входа. Проверьте введённые данные.");
     }
   };
 
@@ -45,125 +43,144 @@ const AuthPage = () => {
         ...signUpData,
         birthdate: new Date(signUpData.birthdate),
       });
-      // setMessage("Успешная регистрация. Токен: " + response.token);
       localStorage.setItem("userToken", response.token);
       router.push("/home");
     } catch (error) {
       console.error(error);
-      setMessage("Ошибка регистрации. Проверьте введённые данные.");
     }
   };
 
+  const handleSignUpClick = () => {
+    setIsRightPanelActive(true);
+  };
+
+  const handleSignInClick = () => {
+    setIsRightPanelActive(false);
+  };
+
   return (
-    <div className={styles["auth-container"]}>
-      <div className={styles["auth-toggle"]}>
-        <button
-          onClick={() => setIsLogin(true)}
-          className={isLogin ? "active" : ""}
-        >
-          Вход
-        </button>
-        <button
-          onClick={() => setIsLogin(false)}
-          className={!isLogin ? "active" : ""}
-        >
-          Регистрация
-        </button>
-      </div>
-      {message && <p className={styles["message"]}>{message}</p>}
-      {isLogin ? (
-        <form onSubmit={handleLoginSubmit} className={styles["auth-form"]}>
-          <div>
-            <label>Email:</label>
-            <br />
-            <input
-              type="email"
-              value={loginData.email}
-              onChange={(e) =>
-                setLoginData({ ...loginData, email: e.target.value })
-              }
-              required
-            />
-          </div>
-          <div>
-            <label>Пароль:</label>
-            <br />
-            <input
-              type="password"
-              value={loginData.password}
-              onChange={(e) =>
-                setLoginData({ ...loginData, password: e.target.value })
-              }
-              required
-            />
-          </div>
-          <button type="submit">Войти</button>
-        </form>
-      ) : (
+    <div
+      className={`${styles.container} ${
+        isRightPanelActive ? styles["right-panel-active"] : ""
+      }`}
+      id="container"
+    >
+      <div
+        className={`${styles["form-container"]} ${styles["sign-up-container"]}`}
+      >
         <form onSubmit={handleSignUpSubmit} className={styles["auth-form"]}>
-          <div>
-            <label>Имя:</label>
-            <br />
-            <input
-              type="text"
-              value={signUpData.name}
-              onChange={(e) =>
-                setSignUpData({ ...signUpData, name: e.target.value })
-              }
-              required
-            />
+          <h1>Создать аккаунт</h1>
+          <div className={styles["social-container"]}>
+            <a href="#" className={styles.social}>
+              <i className="fab fa-facebook-f"></i>
+            </a>
+            <a href="#" className={styles.social}>
+              <i className="fab fa-google-plus-g"></i>
+            </a>
+            <a href="#" className={styles.social}>
+              <i className="fab fa-linkedin-in"></i>
+            </a>
           </div>
-          <div>
-            <label>Фамилия:</label>
-            <br />
-            <input
-              type="text"
-              value={signUpData.surname}
-              onChange={(e) =>
-                setSignUpData({ ...signUpData, surname: e.target.value })
-              }
-              required
-            />
-          </div>
-          <div>
-            <label>Дата рождения:</label>
-            <br />
-            <input
-              type="date"
-              value={signUpData.birthdate}
-              onChange={(e) =>
-                setSignUpData({ ...signUpData, birthdate: e.target.value })
-              }
-              required
-            />
-          </div>
-          <div>
-            <label>Email:</label>
-            <br />
-            <input
-              type="email"
-              value={signUpData.email}
-              onChange={(e) =>
-                setSignUpData({ ...signUpData, email: e.target.value })
-              }
-              required
-            />
-          </div>
-          <div>
-            <label>Пароль:</label>
-            <br />
-            <input
-              type="password"
-              value={signUpData.password}
-              onChange={(e) =>
-                setSignUpData({ ...signUpData, password: e.target.value })
-              }
-              required
-            />
-          </div>
+          <span>или используйте ваш email для регистрации</span>
+          <input
+            type="text"
+            placeholder="Имя"
+            value={signUpData.name}
+            onChange={(e) =>
+              setSignUpData({ ...signUpData, name: e.target.value })
+            }
+            required
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            value={signUpData.email}
+            onChange={(e) =>
+              setSignUpData({ ...signUpData, email: e.target.value })
+            }
+            required
+          />
+          <input
+            type="password"
+            placeholder="Пароль"
+            value={signUpData.password}
+            onChange={(e) =>
+              setSignUpData({ ...signUpData, password: e.target.value })
+            }
+            required
+          />
           <button type="submit">Зарегистрироваться</button>
         </form>
-      )}
+      </div>
+      <div
+        className={`${styles["form-container"]} ${styles["sign-in-container"]}`}
+      >
+        <form onSubmit={handleLoginSubmit} className={styles["auth-form"]}>
+          <h1>Вход</h1>
+          <div className={styles["social-container"]}>
+            <a href="#" className={styles.social}>
+              <i className="fab fa-facebook-f"></i>
+            </a>
+            <a href="#" className={styles.social}>
+              <i className="fab fa-google-plus-g"></i>
+            </a>
+            <a href="#" className={styles.social}>
+              <i className="fab fa-linkedin-in"></i>
+            </a>
+          </div>
+          <span>или используйте ваш аккаунт</span>
+          <input
+            type="email"
+            placeholder="Email"
+            value={loginData.email}
+            onChange={(e) =>
+              setLoginData({ ...loginData, email: e.target.value })
+            }
+            required
+          />
+          <input
+            type="password"
+            placeholder="Пароль"
+            value={loginData.password}
+            onChange={(e) =>
+              setLoginData({ ...loginData, password: e.target.value })
+            }
+            required
+          />
+          <a href="#">Забыли пароль?</a>
+          <button type="submit">Войти</button>
+        </form>
+      </div>
+      <div className={styles["overlay-container"]}>
+        <div className={styles.overlay}>
+          <div
+            className={`${styles["overlay-panel"]} ${styles["overlay-left"]}`}
+          >
+            <h1>С возвращением!</h1>
+            <p>Чтобы оставаться на связи, войдите с вашими личными данными</p>
+            <button
+              className={styles.ghost}
+              onClick={handleSignInClick}
+              id="signIn"
+            >
+              Войти
+            </button>
+          </div>
+          <div
+            className={`${styles["overlay-panel"]} ${styles["overlay-right"]}`}
+          >
+            <h1>Привет, друг!</h1>
+            <p>Введите ваши личные данные и начните путешествие с нами</p>
+            <button
+              className={styles.ghost}
+              onClick={handleSignUpClick}
+              id="signUp"
+            >
+              Зарегистрироваться
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
